becdae90501af62d655ffb6fe66719d519f37ccb
Zach Tibbitts <zachtib@gmail.com>
2016-10-07 19:09:44 -0400

    change serialization for Android compat

--- sampleapi/books/serializers.py
+++ sampleapi/books/serializers.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from .models import Author, Book
 
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Book
-        fields = ('title', 'author', 'pub_date')
-
-
-class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ('first_name', 'last_name')
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Book
+        fields = ('title', 'author', 'pub_date')
